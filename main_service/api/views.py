6ac8fae62240cfabe8ffb6e6399fb473867177a0
Андрей Зюзин <andrey.bp.zyuzin@yandex.ru>
2023-09-12 16:19:48 +0300

    working

--- main_service/api/views.py
+++ main_service/api/views.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 
+import requests
 from rest_framework import viewsets
 from django.http import JsonResponse
 
 from cadastral.models import Query
 from api.serializers import QuerySerializer, ResultSerializer
+
+
+EXTERNAL_RESULT = 'http://localhost:8001/result/'
+EXTERNAL_PING = 'http://localhost:8001/'
 
 
 class QueryViewSet(viewsets.ModelViewSet):
@@ -14,7 +19,10 @@ class QueryViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(time_came=datetime.now())
-        res = {'result': 42}
+        print(serializer.data)
+        print('go')
+        res = requests.post(EXTERNAL_RESULT, data=serializer.data)
+        print(res)
         serializer.save(result_response=res['result'],
                         time_went=datetime.now())
 

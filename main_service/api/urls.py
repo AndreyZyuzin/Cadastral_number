1a0c877014340a8b5d3b9e312e62ca87a2a51daf
Андрей Зюзин <andrey.bp.zyuzin@yandex.ru>
2023-09-13 09:27:44 +0300

    done result and history

--- main_service/api/urls.py
+++ main_service/api/urls.py
@@ -1,10 +1,9 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-
 from django.urls import path
 
-from api.views import QueryViewSet
+from api.views import QueryViewSet, ResultDetail, HistoryList
 
 urlpatterns = [
     path('query/', QueryViewSet.as_view({'post': 'create'}), name='query'),
+    path('result/<int:pk>/', ResultDetail.as_view(), name='result_detail'),
+    path('history/', HistoryList.as_view(), name='history_list')
 ]
--- main_service/api/views.py
+++ main_service/api/views.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 import requests
 from rest_framework import viewsets
-from django.http import JsonResponse
+from rest_framework import generics
 
 from cadastral.models import Query
 from api.serializers import QuerySerializer, ResultSerializer
@@ -18,15 +18,24 @@ class QueryViewSet(viewsets.ModelViewSet):
     http_method_names = ['post']
 
     def perform_create(self, serializer):
-        serializer.save(time_came=datetime.now())
+        query = serializer.save(time_came=datetime.now())
+        query_id = query.id
         print(serializer.data)
-        print('go')
         res = requests.post(EXTERNAL_RESULT, data=serializer.data)
-        print(res)
-        serializer.save(result_response=res['result'],
-                        time_went=datetime.now())
+        res = res.json()
+        query = Query.objects.get(id=query_id)
+        print(f'res: {res.get("result")}')
+        query.result_response = res.get('result')
+        print(f'res: {datetime.now()}')
+        query.time_went=datetime.now()
+        query.save()
 
 
-class ResultViewSet(viewsets.ModelViewSet):
+class ResultDetail(generics.RetrieveAPIView):
+    queryset = Query.objects.all()
+    serializer_class = ResultSerializer
+
+
+class HistoryList(generics.ListAPIView):
     queryset = Query.objects.all()
     serializer_class = ResultSerializer
